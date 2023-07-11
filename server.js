8a9efedeb845e6abcc849ff819146dd407744437
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-07-11 14:59:00 +0900

    HTML 생성

--- server.js
+++ server.js
@@ -30,3 +30,7 @@ app.get('/pet', function(요청, 응답){
 app.get('/', function(요청, 응답){
     응답.sendFile(__dirname + '/index.html');
 });
+
+app.get('/write', function(요청, 응답){
+    응답.sendFile(__dirname + '/write.html');
+});
