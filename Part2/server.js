9241341ef1d33d910d6ddf6087a736ddf6db4fda
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-09-26 09:45:55 +0900

    mondodb 연결

--- Part2/server.js
+++ Part2/server.js
@@ -3,9 +3,16 @@ const app = express();
 const bodyParer = require('body-parser');
 app.use(bodyParer.urlencoded({ extended: true }));
 
-http.listen(8080, function () {
-    console.log('listening on 8080')
-});
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://bestfesu:skshfpel7@cluster0.lbgqxym.mongodb.net/?retryWrites=true&w=majority',
+    function (에러, client) {
+        // database 접속이 완료되면, 내부 코드 실행하기 
+        http.listen(8080, function () {
+            console.log('listening on 8080')
+        });
+    })
+
+
 
 app.post('/add', function (요청, 응답) {
     응답.send('전송완료');
