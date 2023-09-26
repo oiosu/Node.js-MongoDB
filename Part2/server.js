88a0cbdc7b51f3c1a89033229ddfef7ae0347017
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-09-26 12:09:25 +0900

    MongoDB 오류 해결 진행중

--- Part2/server.js
+++ Part2/server.js
@@ -1,21 +1,26 @@
-const express = require('express');
-const app = express();
-const bodyParer = require('body-parser');
-app.use(bodyParer.urlencoded({ extended: true }));
-
-const MongoClient = require('mongodb').MongoClient;
-MongoClient.connect('mongodb+srv://bestfesu:skshfpel7@cluster0.lbgqxym.mongodb.net/?retryWrites=true&w=majority',
-    function (에러, client) {
-        // database 접속이 완료되면, 내부 코드 실행하기 
-        http.listen(8080, function () {
-            console.log('listening on 8080')
-        });
-    })
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+app.use(express.urlencoded({ extended: true }));
 
 
+var db;
+MongoClient.connect('접속URL', { useUnifiedTopology: true }, function (에러, client) {
+    if (에러) return console.log(에러)
+    db = client.db('todoapp');
 
-app.post('/add', function (요청, 응답) {
-    응답.send('전송완료');
-    console.log(요청.body.data);
-    console.log(요청.body.title);
-});
+    db.collection('post').insertOne({ 이름: 'John', _id: 100 }, function (에러, 결과) {
+        console.log('저장완료');
+    });
+
+    app.listen(8080, function () {
+        console.log('listening on 8080')
+    });
+});;
+
+
+// app.post('/add', function (요청, 응답) {
+//     응답.send('전송완료');
+//     console.log(요청.body.data);
+//     console.log(요청.body.title);
+// });
